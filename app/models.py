022c6209fb2de17a1716b66c28c4d62794a1fa33
Nitin Prakash <prakash.nitin63@gmail.com>
2017-10-05 00:01:38 +0530

    update requirements

--- app/models.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from app import db
-
-class User(db.Document):
-	email = db.EmailField(unique=True)
-	password = db.StringField(default=True)
-	active = db.BooleanField(default=True)
-	isAdmin = db.BooleanField(default=False)
-	timestamp = db.DateTimeField(default=datetime.datetime.now())
