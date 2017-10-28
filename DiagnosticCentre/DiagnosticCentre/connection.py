f80e61a78ac401577fa2b3fcd82d61c09e331891
Nitin Prakash <prakash.nitin63@gmail.com>
2017-10-28 19:18:06 +0530

    update credentials

--- DiagnosticCentre/DiagnosticCentre/connection.py
+++ DiagnosticCentre/DiagnosticCentre/connection.py
@@ -1,8 +1,15 @@
 from cloudant.client import Cloudant
 
+#conn = None
+
 def create():
-	cloudant_user = "f95c6130-1964-448c-a8d7-257d80b30e65-bluemix"
-	cloudant_pass = "9aabf072adfc481b889e322f7824c79b4c517eb187a447bf7fba90120b278c1d"
-	cloudant_url = "https://f95c6130-1964-448c-a8d7-257d80b30e65-bluemix:9aabf072adfc481b889e322f7824c79b4c517eb187a447bf7fba90120b278c1d@f95c6130-1964-448c-a8d7-257d80b30e65-bluemix.cloudant.com"
+	global conn
+	cloudant_user = "956570a2-4054-4fee-8dc6-bb699d1022d3-bluemix"
+	cloudant_pass = "e1d8852c83e0cf2201b54ff7401058db52fa85f6d6fed8a9c2b2659ff66ff9cd"
+	cloudant_url = "https://956570a2-4054-4fee-8dc6-bb699d1022d3-bluemix:e1d8852c83e0cf2201b54ff7401058db52fa85f6d6fed8a9c2b2659ff66ff9cd@956570a2-4054-4fee-8dc6-bb699d1022d3-bluemix.cloudant.com"
+
 	client = Cloudant(cloudant_user, cloudant_pass, url=cloudant_url, connect=True, auto_renew=True)
 	return client
+
+def close():
+	conn.close()
